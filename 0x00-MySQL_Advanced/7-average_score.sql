7fe790ad8605de6cbb58bf74c07da52c53e5cdc1
wiseman-umanah <wisemanumanah@gmail.com>
2024-06-13 22:15:48 +0100

    Fixed up code to pass test

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,19 +1,12 @@
--- Write a SQL script that creates a stored procedure ComputeAverageScoreForUser
--- that computes and store the average score for a student.
-DELIMITER $
-
-CREATE PROCEDURE ComputeAverageScoreForUser(IN p_user_id INT)
+-- SQL script that creates a stored procedure ComputeAverageScoreForUser
+-- that computes and store the overall score for a student.
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT)
 BEGIN
-    SELECT u.id AS id, u.name AS name, AVG(c.score) AS average_score
-    FROM 
-        corrections c
-    JOIN 
-        users u ON c.user_id = u.id
-    WHERE
-        c.user_id = p_user_id
-    GROUP BY 
-        u.id, u.name;
+    DECLARE avg_score FLOAT;
+    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
 END
-$
-
+$$
 DELIMITER ;
