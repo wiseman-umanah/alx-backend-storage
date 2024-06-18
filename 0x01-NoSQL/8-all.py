bb8f8d9ec50b1fddc8be587e7aad8ec28cc2d1a1
wiseman-umanah <wisemanumanah@gmail.com>
2024-06-18 08:59:22 +0100

    Fixed up 8 python

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+"""a Python function that
+lists all documents in a collection"""
 from typing import List, Dict
 from pymongo.collection import Collection  # Import Collection from pymongo
 
