19acd046245e0047cf1c1516bb3137d970938a8d
wiseman-umanah <wisemanumanah@gmail.com>
2024-06-19 07:37:05 +0100

    Fixed up log stats problem

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -16,17 +16,29 @@ def main(mongo_collection):
     print("{} logs".format(mongo_collection.count_documents({})))
     print("Methods:")
     for i in method:
-        print(f"\tmethod {i}: {mongo_collection.count_documents(
-            {"method": i})}")
-    print(mongo_collection.count_documents({"path": "/status"},
-                                           {"method": "GET"}), "status check")
+        print("\tmethod {}: {}".format(i, mongo_collection.count_documents(
+            {'method': i})))
+    status = mongo_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print("{} status check".format(status))
     top_ips = mongo_collection.aggregate([
-        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$group":
+         {
+             "_id": "$ip",
+             "count": {"$sum": 1}
+         }
+         },
         {"$sort": {"count": -1}},
-        {"$limit": 10}])
+        {"$limit": 10},
+        {"$project": {
+            "_id": 0,
+            "ip": "$_id",
+            "count": 1
+        }}
+    ])
     print("IPs:")
     for t in top_ips:
-        print(f"\t{i._id}: {i.count}")
+        print(f"\t{t.get('ip')}: {t.get('count')}")
 
 
 if __name__ == "__main__":
--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -16,10 +16,11 @@ def main(mongo_collection):
     print("{} logs".format(mongo_collection.count_documents({})))
     print("Methods:")
     for i in method:
-        print(f"\tmethod {i}: {mongo_collection.count_documents(
-            {"method": i})}")
-    print(mongo_collection.count_documents({"path": "/status"},
-                                           {"method": "GET"}), "status check")
+        print("\tmethod {}: {}".format(i, mongo_collection.count_documents(
+            {'method': i})))
+    status = mongo_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print("{} status check".format(status))
 
 
 if __name__ == "__main__":
